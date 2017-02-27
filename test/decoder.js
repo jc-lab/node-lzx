9dc4bc963274418dee42ec65c83b8ffd4049045a
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-02-28 00:37:59 +0100

    test: Fix option test values

--- test/decoder.js
+++ test/decoder.js
@@ -10,15 +10,15 @@ describe( 'Decoder', function() {
     })
 
     specify( 'window size must be larger than MIN_WINDOW_SIZE', function() {
-      assert.throws(() => { new LZX.Decoder({ windowSize: LZX.MIN_WINDOW_SIZE - 1 }) })
+      assert.throws(() => { new LZX.Decoder({ windowSize: LZX.MIN_WINDOW_SIZE / 2 }) })
     })
 
     specify( 'window size must be smaller than MAX_WINDOW_SIZE', function() {
-      assert.throws(() => { new LZX.Decoder({ windowSize: LZX.MAX_WINDOW_SIZE + 1 }) })
+      assert.throws(() => { new LZX.Decoder({ windowSize: LZX.MAX_WINDOW_SIZE * 2 }) })
     })
 
     specify( 'reject non-power-of-two window size', function() {
-      assert.throws(() => { new LZX.Decoder({ windowSize: 1234 }) })
+      assert.throws(() => { new LZX.Decoder({ windowSize: LZX.MIN_WINDOW_SIZE + 2 }) })
     })
 
   })
--- test/encoder.js
+++ test/encoder.js
@@ -6,15 +6,15 @@ describe( 'Encoder', function() {
   context( 'options', function() {
 
     specify( 'window size must be larger than MIN_WINDOW_SIZE', function() {
-      assert.throws(() => { new LZX.Encoder({ windowSize: LZX.MIN_WINDOW_SIZE - 1 }) })
+      assert.throws(() => { new LZX.Encoder({ windowSize: LZX.MIN_WINDOW_SIZE / 2 }) })
     })
 
     specify( 'window size must be smaller than MAX_WINDOW_SIZE', function() {
-      assert.throws(() => { new LZX.Encoder({ windowSize: LZX.MAX_WINDOW_SIZE + 1 }) })
+      assert.throws(() => { new LZX.Encoder({ windowSize: LZX.MAX_WINDOW_SIZE * 2 }) })
     })
 
     specify( 'reject invalid non-power-of-two window size', function() {
-      assert.throws(() => { new LZX.Encoder({ windowSize: 1234 }) })
+      assert.throws(() => { new LZX.Encoder({ windowSize: LZX.MIN_WINDOW_SIZE + 2 }) })
     })
 
   })
